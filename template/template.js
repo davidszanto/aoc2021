60e09f71b7a20f2d167e511449f593ec6f0b2748
David <szavid0@gmail.com>
2021-12-02 08:59:52 +0100

    Template added

---
+++ template/template.js
@@ -0,0 +1,18 @@
+import { getLinesFromText } from "../utils.js";
+
+const first = (data) => {
+    const lines = getLinesFromText(data);
+
+    //TODO
+}
+
+const second = (data) => {
+    const lines = getLinesFromText(data);
+
+    //TODO
+}
+
+export {
+    first as templateFirst,
+    second as templateSecond
+}
